55da7082f3e0b9fcf0a95147cc43a8fb3e81196e
Lee Edwards <lee@root.vc>
2021-03-14 09:53:24 -0700

    Add Firebase config and lockfiles

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyA7ClfRiPefrovi3b59wswssLHhr1_03WQ",
+  authDomain: "rootvc-octopus-wiki.firebaseapp.com",
+  projectId: "rootvc-octopus-wiki",
+  storageBucket: "rootvc-octopus-wiki.appspot.com",
+  messagingSenderId: "343041375530",
+  appId: "1:343041375530:web:1b8d7fbc2c903ddd957253",
+  measurementId: "G-XDXQQWNEHJ"
+};
